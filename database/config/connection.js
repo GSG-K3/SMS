c366ab26dac40e0da8d1615e04bf683b0fb47ae6
kholoudfann <141060@ppu.edu.ps>
2020-02-26 10:07:28 +0200

    connection the schema sql with build.js #22

--- database/config/connection.js
+++ database/config/connection.js
@@ -1,19 +1,26 @@
-require("env2")("./config.env");
-const { Pool } = require("pg");
-const url = '';
+const {Pool} = require('pg');
+const url = require('url');
+const env = require('env2');
+env('./config.env');
 
-let connectionString = process.env.DB_URL;
-
-if (process.env.NODE_ENV === "test") {
-  connectionString = process.env.TEST_DB_URL;
+if (!process.env.DB_URL) {
+  throw new Error('Environment variable DATABASE_URL must be set');
 }
-if (!connectionString) {
-  throw new Error("please set a DB_URL env variable");
 
-}
+const params = url.parse(process.env.DB_URL);
+
+const [username, password] = params.auth.split(':');
+
 const options = {
-    connectionString: url,
-    sll: true
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  max: process.env.DB_MAX_CONNECTIONS || 2
 };
+
+if (username) { options.user = username; }
+if (password) { options.password = password; }
+
+options.ssl = (options.host !== 'localhost');
 
 module.exports = new Pool(options);
